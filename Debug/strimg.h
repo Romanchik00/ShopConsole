37dfee921537d8977d99836ed261e86b820649cb
chamomile-man <romavkube@gmail.com>
2025-01-26 18:29:03 +0300

    Add files via upload

---
+++ Debug/strimg.h
@@ -0,0 +1,11 @@
+#pragma once
+//#define Hello "Hello"
+extern const char man[], female[], beer[], wine[], apple[], orange[];
+extern const char banana[], pear[], god[], box[], test[];
+
+void TitlePage();
+//void mainmenu();
+//void base();
+//void Stuffbase(Stuff personal[]);
+
+//void Card();
